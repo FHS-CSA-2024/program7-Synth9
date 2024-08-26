5899944f79baf9e0620afab076aa1f08a8cbb7e8
Synth9 <96803186+Synth9@users.noreply.github.com>
2024-08-26 15:23:37 -0700

    Update Program.java
    Finished Program7

--- Program.java
+++ Program.java
@@ -1,9 +1,46 @@
-//import stuff here
+import java.util.Scanner;
+
+public class Program7{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter schrute-bucks: ");
+        int schruteBucksInput = input.nextInt();
+        
+        System.out.println("Enter klevins: ");
+        int klevinsInput= input.nextInt();
+        
+        System.out.println("Enter stanley-nickels: ");
+        int stanleyNickelsInput = input.nextInt();
+        
+        final double schruteBucksConversion = 1.0;
+        final double klevinsConversion = 0.05;
+        final double stanleyNickelsConversion = 0.05 / 12;
+        
+        double schruteBucks = schruteBucksConversion * schruteBucksInput;
+        double klevins = klevinsConversion * klevinsInput;
+        double stanleyNickels = stanleyNickelsConversion * stanleyNickelsInput;
+        
+        schruteBucks = schruteBucks + klevins + stanleyNickels;
+        
+        //Rounding
+        schruteBucks = schruteBucks * 100;
+        schruteBucks = Math.round(schruteBucks);
+        schruteBucks = schruteBucks / 100;
+        
+        System.out.println("Decimal schrute-bucks = $" + schruteBucks);
+    }
+}//import stuff here
 
 //Your code here
 
 //Paste console output below:
 /*
-
+Enter schrute-bucks: 
+7
+Enter klevins: 
+17
+Enter stanley-nickels: 
+9
+Decimal schrute-bucks = $7.89
 
 */
